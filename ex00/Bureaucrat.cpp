cdf2ee3a358c69800b33f6a4819535fee18e2092
Numbersdontlie <luifer92@gmail.com>
2025-06-26 00:44:54 +0200

    Adding the methods for increase and decrease and the exception implementation

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 00:30:16 by luifer            #+#    #+#             */
-/*   Updated: 2025/06/26 00:38:26 by luifer           ###   ########.fr       */
+/*   Updated: 2025/06/26 00:44:13 by luifer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,36 @@ const std::string& Bureaucrat::getName() const {
 
 int Bureaucrat::getName() const {
     return this->_grade;
+}
+
+// Methods to increase or decrease
+// method to increase the grade
+void Bureaucrat::increaseGrade() {
+    if (this->_grade <= _minGrade)
+        throw GradeTooHighException();
+    this->_grade--;
+}
+
+// method to decrease the grade
+void Bureaucrat::decreaseGrade() {
+    if (this->_grade >= _maxGrade)
+        throw GradeTooHighException();
+    this->_grade++;
+}
+
+// Exception classes
+// This exception is thrown when the grade is too high
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+    return ("Grade is too high! It must be between 1 and 150 pal.");
+}
+
+// This exception is thrown when the grade is too low
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+    return ("Grade is too low! It must be between 1 and 150 pal.");
+}
+
+// This operator overload allows us to print the Bureaucrat object directly using std::cout
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
+    os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+    return os;
 }
