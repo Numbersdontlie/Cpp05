696e8296d38ced9e92f6f1a4dac9ff40af7984cd
Numbersdontlie <luifer92@gmail.com>
2025-06-26 17:44:21 +0200

    Working on exercise 1 -> Form class created and methods defined in cpp file

---
+++ ex01/Form.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 14:41:05 by luifer            #+#    #+#             */
+/*   Updated: 2025/06/26 17:43:38 by luifer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+// constructor with parameters
+Form::Form(const std::string &name, int signGrade, int execGrade): _name(name), _isSigned(false), _signGrade(signGrade), _executeGrade(execGrade) {
+    std::cout << BLUE << "Form constructed called" << RESET << std::endl;
+    if (signGrade < Bureaucrat::_minGrade || execGrade < Bureaucrat::_minGrade)
+        throw GradeTooHighException();
+    if (signGrade > Bureaucrat::_maxGrade || execGrade > Bureaucrat::_maxGrade)
+        throw GradeTooLowException();
+}
+
+// copy constructor
+Form::Form(const Form &other): _name(other._name), _isSigned(other._isSigned), _signGrade(other._signGrade), _executeGrade(other._executeGrade) {
+    std::cout << BLUE << "Form copy constructor called" << RESET << std::endl;
+}
+
+
+// destructor
+Form::~Form() {
+    std::cout << RED << "Form destructor called" << RESET << std::endl;
+}
+
+// getters
+const std::string& Form::getName() const {
+    return _name;
+}
+
+bool Form::getSigned() const {
+    return _isSigned;
+}
+
+int Form::getSignGrade() const {
+    return _signGrade;
+}
+
+int Form::getExecuteGrade() const {
+    return _executeGrade;
+}
+
+// methods for the Form class
+void Form::beSigned(const Bureaucrat &bureaucrat) {
+    if (bureaucrat.getGrade() > _signGrade)
+        throw GradeTooLowException();
+    this->_isSigned = true;
+}
+
+const char* Form::GradeTooHighException::what() const throw() {
+    return "Form: Grade too high";
+}
+
+const char* Form::GradeTooLowException::what() const throw() {
+    return "Form: Grade too low";
+}
+
+// Overloading the output operator
+std::ostream& operator<<(std::ostream &os, const Form &form) {
+    os << "Form Name: " << form.getName() << ", Signed: " << (form.getSigned() ? "Yes" : "No")
+       << ", Sign Grade: " << form.getSignGrade() << ", Execute Grade: " << form.getExecuteGrade();
+    return os;
+}
---
+++ ex01/Form.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 13:52:20 by luifer            #+#    #+#             */
+/*   Updated: 2025/06/26 14:30:01 by luifer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include "Bureaucrat.hpp"
+
+#define BLUE "\e[1;94m"
+#define RED "\e[1;91m"
+#define GREEN "\e[1;92m"
+#define RESET "\033[0m"
+
+class Form {
+    private:
+        // They are private to prevent instantiation without parameters
+        Form(); // Default constructor
+        Form &operator=(const Form &other); // Assignment operator 
+        const std::string _name;
+        bool _isSigned;
+        const int _signGrade;
+        const int _executeGrade;
+    
+    public:
+        Form(const std::string &name, int signGrade, int execGrade);
+        Form(const Form &other);
+        ~Form();
+
+        // Getters
+        const std::string& getName() const;
+        bool getSigned() const;
+        int getSignGrade() const;
+        int getExecuteGrade() const;
+
+        // Methods
+        void beSigned(const Bureaucrat &bureaucrat);
+
+        //Exceptions classes
+        class GradeTooHighException: public std::exception {
+            public:
+                const char* what() const throw(); 
+        };
+        class GradeTooLowException: public std::exception {
+            public:
+                const char* what() const throw();
+        };
+};
+
+std::ostream& operator<<(std::ostream &os, const Form &form);
+
+#endif
