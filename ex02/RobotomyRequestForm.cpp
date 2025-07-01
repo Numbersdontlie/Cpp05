9e426b2264ef5620cc859ee5d3b4a8cad13eae27
Numbersdontlie <luifer92@gmail.com>
2025-07-01 23:15:32 +0200

    Adding methods to the robotomy class

--- ex02/RobotomyRequestForm.cpp
+++ ex02/RobotomyRequestForm.cpp
@@ -6,14 +6,39 @@
 /*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 23:39:06 by luifer            #+#    #+#             */
-/*   Updated: 2025/06/26 23:47:03 by luifer           ###   ########.fr       */
+/*   Updated: 2025/07/01 23:15:09 by luifer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RobotomyRequestForm.hpp"
 
+//Default constructor
 RobotomyRequestForm::RobotomyRequestForm(): AForm("RobotomyRequestForm", 72, 45), _target("default_target") {
     std::cout << BLUE << "RobotomyRequestForm default constructor called 🤖 🤖 🤖" << RESET << std::endl;
 }
 
-RobotomyRequestForm::Robotomy
+// Parameterized constructor
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target): AForm("RobotomyRequestForm", 72, 45), _target("default_target") {
+    std::cout << BLUE << "RobotomyRequestForm parameterized constructor called 🤖 🤖 🤖" << RESET << std::endl;
+    this->_target = target;
+}
+
+// Copy constructor
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &other): AForm("RobotomyRequestForm", 72, 45), {
+    std::cout << BLUE << "RobotomyRequestForm copy constructor called 🤖 🤖 🤖" << RESET << std::endl;
+    this->_target = other._target;
+}
+
+// Assignment operator
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &other) {
+    std::cout << BLUE << "RobotomyRequestForm assignment operator called 🤖 🤖 🤖" << RESET << std::endl;
+    if (this != &other) {
+        this->_target = other._target;
+    }
+    return *this;
+}
+
+//Destructor
+RobotomyRequestForm::~RobotomyRequestForm() {
+    std::cout << RED << "RobotomyRequestForm destructor called 🤖 🤖 🤖" << RESET << std::endl;
+}
